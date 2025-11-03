fc2e296320bebf13b0dedf3700ca2b66bfeee235
Chayan Maji <chayanmaji@gmail.com>
2025-11-03 00:28:40 -0500

    Create a new route to get a security token

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import 'dotenv/config'
+
 const app = express();
+app.use(express.json());
 
 const posts = [{
     username: 'chayan',
@@ -13,6 +17,13 @@ const posts = [{
 app.get('/posts', (req, res) => {
     res.json(posts);
 });
+
+app.post('/login', (req, res) => {
+    
+    const user = { name : req.body.username  };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken });
+})
 
 const port = 3000;
 app.listen(port , () => {
