554c790c858486ae92a5cd49bcbbe56f9ff5e917
Chayan Maji <chayanmaji@gmail.com>
2025-11-03 01:07:27 -0500

    secured the 'posts' end point

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const posts = [{
 }];
 
 
-app.get('/posts', (req, res) => {
-    res.json(posts);
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name));
 });
 
 app.post('/login', (req, res) => {
@@ -24,6 +24,21 @@ app.post('/login', (req, res) => {
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
     res.json({ accessToken });
 })
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    if (authHeader === undefined || authHeader === null) return res.sendStatus(401);
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token === null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+
+}
 
 const port = 3000;
 app.listen(port , () => {
